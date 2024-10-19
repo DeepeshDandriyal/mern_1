66ee1b58ebcc21602bed1a23add18e69186e3b39
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2024-10-19 11:01:07 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routes/product.route.js";
 dotenv.config();
@@ -7,9 +8,21 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 app.use(express.json()); //allow us to accept json data in body
 
 app.use("/api/products", productRouter);
+
+//to make same url for frontend and backend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(5000, () => {
   connectDB();
